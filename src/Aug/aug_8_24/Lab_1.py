b768addacf66a53c6000ba95c1fb310a76992435
jayur642 <jayur642@gmail.com>
2024-09-28 17:34:04 +0530

    files added

--- src/Aug/aug_8_24/Lab_1.py
+++ src/Aug/aug_8_24/Lab_1.py
@@ -1,2 +1,25 @@
-print('Hello')
-print('Hello') # tap ctrl d to duplicate
+# print('Hello')
+# print('Hello') # tap ctrl d to duplicate
+
+
+list1 = [1,2,3,4,5,6,7]
+def sum_even_odd(list1):
+    sum_even = 0
+    sum_odd = 0
+
+    for i in list1:
+        if i % 2 == 0:
+            sum_even += i
+        else:
+            sum_odd += i
+    return sum_even, sum_odd
+
+even_sum, odd_sum = sum_even_odd(list1)
+print("Sum of even numbers:", even_sum)
+print("Sum of odd numbers:", odd_sum)
+
+
+
+
+
+
